d57a42a9f72a7f3fdc343f0f0baccaeca1fe2390
abstractmagicalwand <abstractmagicalwand@gmail.com>
2019-06-20 12:00:57 +0300

    Implement chat

--- 9-module/2-task/controllers/messages.js
+++ 9-module/2-task/controllers/messages.js
@@ -1,5 +1,9 @@
 const Message = require('../models/Message');
 
 module.exports = async function messages(ctx, next) {
-  return [];
+  let messages = await Message.find().populate('user');
+  messages = messages.map(({_id: id, date, text, user: {displayName: user}}) =>
+    ({id, date, text, user}));
+
+  ctx.body = {messages};
 };
--- 9-module/2-task/socket.js
+++ 9-module/2-task/socket.js
@@ -5,15 +5,40 @@ const Message = require('./models/Message');
 
 function socket(server) {
   const io = socketIO(server);
-  
+
   io.use(async function(socket, next) {
+    const {token} = socket.handshake.query;
+    if (!token) {
+      next(new Error('anonymous sessions are not allowed'));
+      return;
+    }
+
+    const session = await Session.findOne({token}).populate('user');
+    if (!session) {
+      next(new Error('wrong or expired session token'));
+      return;
+    }
+
+    socket.user = session.user;
     next();
   });
 
-  io.on('connection', function (socket) {
-    socket.on('message', (msg) => {});
+  io.on('connection', function(socket) {
+    socket.on('message', async (text) => {
+      const date = new Date();
+      socket.emit(
+          'user_message',
+          {
+            user: socket.user.displayName,
+            text,
+            date,
+          }
+      );
+
+      await Message.create({user: socket.user, text, date});
+    });
   });
-  
+
   return io;
 }
 
